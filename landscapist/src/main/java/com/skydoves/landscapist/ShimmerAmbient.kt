71aa5f4272b48e9b24f788fb8bf35d13fb085146
skydoves <skydoves@naver.com>
2020-11-14 15:33:30 +0900

    Refactor ShimmerParamsAmbient to providing non-nullable default shimmerparams

--- landscapist/src/main/java/com/skydoves/landscapist/ShimmerAmbient.kt
+++ landscapist/src/main/java/com/skydoves/landscapist/ShimmerAmbient.kt
@@ -16,26 +16,25 @@
 
 @file:JvmName("ShimmerParamsAmbient")
 @file:JvmMultifileClass
+@file:Suppress("unused")
 
 package com.skydoves.landscapist
 
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ambientOf
 import androidx.compose.ui.graphics.Color
+import com.skydoves.landscapist.ShimmerAmbientProvider.defaultShimmerParams
 
 /**
  * Ambient containing the preferred [ShimmerParams] for providing the same instance
  * in the composable hierarchy.
  */
-val ShimmerParamsAmbient = ambientOf<ShimmerParams?> { null }
+val ShimmerParamsAmbient = ambientOf { defaultShimmerParams() }
 
 /** A provider for taking the ambient instances related to the [Shimmer]. */
 object ShimmerAmbientProvider {
 
   /** Returns the current or default [ShimmerParams] for the `shimmerParams` parameter. */
-  @Composable
-  fun getShimmerParams(): ShimmerParams {
-    return ShimmerParamsAmbient.current
-      ?: ShimmerParams(baseColor = Color.DarkGray, highlightColor = Color.LightGray)
+  fun defaultShimmerParams(): ShimmerParams {
+    return ShimmerParams(baseColor = Color.DarkGray, highlightColor = Color.LightGray)
   }
 }
