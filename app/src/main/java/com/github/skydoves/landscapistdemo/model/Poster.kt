937ef61c281f2c9331bbff22797e538e8537cff6
skydoves <skydoves@naver.com>
2020-12-03 21:07:17 +0900

    Remove unused kotlin-android-extension plugin

--- app/src/main/java/com/github/skydoves/landscapistdemo/model/Poster.kt
+++ app/src/main/java/com/github/skydoves/landscapistdemo/model/Poster.kt
@@ -16,16 +16,13 @@
 
 package com.github.skydoves.landscapistdemo.model
 
-import android.os.Parcelable
 import androidx.compose.runtime.Immutable
-import kotlinx.android.parcel.Parcelize
 
 @Immutable
-@Parcelize
 data class Poster(
   val name: String,
   val release: String,
   val playtime: String,
   val description: String,
   val poster: String
-) : Parcelable
+)
