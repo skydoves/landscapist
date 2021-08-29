9763000f8c0f2764828ac452e17db4540f56510a
skydoves <skydoves@naver.com>
2021-08-29 23:17:03 +0900

    Refactor DrawablePainter and add rememberDrawablePainter

--- landscapist/src/main/java/com/skydoves/landscapist/DrawablePainter.kt
+++ landscapist/src/main/java/com/skydoves/landscapist/DrawablePainter.kt
@@ -20,11 +20,15 @@ import android.graphics.drawable.Animatable
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Handler
 import android.os.Looper
 import android.view.View
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.RememberObserver
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
@@ -39,7 +43,6 @@ import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.withSave
 import androidx.compose.ui.unit.LayoutDirection
-import androidx.core.graphics.drawable.DrawableCompat
 import kotlin.math.roundToInt
 
 /**
@@ -49,14 +52,13 @@ import kotlin.math.roundToInt
  *
  * Taken from https://goo.gle/compose-drawable-painter
  */
-internal class AndroidDrawablePainter(
-  private val drawable: Drawable
-) : Painter() {
+internal class DrawablePainter(
+  val drawable: Drawable
+) : Painter(), RememberObserver {
   private var invalidateTick by mutableStateOf(0)
-  private var startedAnimatable = drawable is Animatable && drawable.isRunning
 
-  init {
-    drawable.callback = object : Drawable.Callback {
+  private val callback: Drawable.Callback by lazy {
+    object : Drawable.Callback {
       override fun invalidateDrawable(d: Drawable) {
         // Update the tick so that we get re-drawn
         invalidateTick++
@@ -72,6 +74,25 @@ internal class AndroidDrawablePainter(
     }
   }
 
+  init {
+    // Update the drawable's bounds to match the intrinsic size
+    drawable.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
+  }
+
+  override fun onRemembered() {
+    drawable.callback = callback
+    drawable.setVisible(true, true)
+    if (drawable is Animatable) drawable.start()
+  }
+
+  override fun onAbandoned() = onForgotten()
+
+  override fun onForgotten() {
+    if (drawable is Animatable) drawable.stop()
+    drawable.setVisible(false, false)
+    drawable.callback = null
+  }
+
   override fun applyAlpha(alpha: Float): Boolean {
     drawable.alpha = (alpha * 255).roundToInt().coerceIn(0, 255)
     return true
@@ -83,13 +104,15 @@ internal class AndroidDrawablePainter(
   }
 
   override fun applyLayoutDirection(layoutDirection: LayoutDirection): Boolean {
-    return DrawableCompat.setLayoutDirection(
-      drawable,
-      when (layoutDirection) {
-        LayoutDirection.Ltr -> View.LAYOUT_DIRECTION_LTR
-        LayoutDirection.Rtl -> View.LAYOUT_DIRECTION_RTL
-      }
-    )
+    if (Build.VERSION.SDK_INT >= 23) {
+      return drawable.setLayoutDirection(
+        when (layoutDirection) {
+          LayoutDirection.Ltr -> View.LAYOUT_DIRECTION_LTR
+          LayoutDirection.Rtl -> View.LAYOUT_DIRECTION_RTL
+        }
+      )
+    }
+    return false
   }
 
   override val intrinsicSize: Size
@@ -99,26 +122,14 @@ internal class AndroidDrawablePainter(
     )
 
   override fun DrawScope.onDraw() {
-    if (!startedAnimatable && drawable is Animatable && !drawable.isRunning) {
-      // If the drawable is Animatable, start it on the first draw
-      drawable.start()
-      startedAnimatable = true
-    }
-
     drawIntoCanvas { canvas ->
       // Reading this ensures that we invalidate when invalidateDrawable() is called
       invalidateTick
 
-      drawable.setBounds(0, 0, size.width.toInt(), size.height.toInt())
+      // Update the Drawable's bounds
+      drawable.setBounds(0, 0, size.width.roundToInt(), size.height.roundToInt())
 
       canvas.withSave {
-        // Painters are responsible for scaling content to meet the canvas size
-        if (drawable.intrinsicWidth > 0 && drawable.intrinsicHeight > 0) {
-          canvas.scale(
-            sx = size.width / drawable.intrinsicWidth,
-            sy = size.height / drawable.intrinsicHeight
-          )
-        }
         drawable.draw(canvas.nativeCanvas)
       }
     }
@@ -130,11 +141,27 @@ private val MAIN_HANDLER by lazy(LazyThreadSafetyMode.NONE) {
 }
 
 /**
- * Allows wrapping of a [Drawable] into a [Painter], attempting to un-wrap the drawable contents
- * and use Compose primitives where possible.
+ * Remembers [Drawable] wrapped up as a [Painter]. This function attempts to un-wrap the
+ * drawable contents and use Compose primitives where possible.
+ *
+ * If the provided [drawable] is `null`, an empty no-op painter is returned.
+ *
+ * This function tries to dispatch lifecycle events to [drawable] as much as possible from
+ * within Compose.
  */
-public fun Drawable.toPainter(): Painter = when (this) {
-  is BitmapDrawable -> BitmapPainter(bitmap.asImageBitmap())
-  is ColorDrawable -> ColorPainter(Color(color))
-  else -> AndroidDrawablePainter(mutate())
+@Composable
+public fun rememberDrawablePainter(drawable: Drawable?): Painter = remember(drawable) {
+  when (drawable) {
+    null -> EmptyPainter
+    is BitmapDrawable -> BitmapPainter(drawable.bitmap.asImageBitmap())
+    is ColorDrawable -> ColorPainter(Color(drawable.color))
+    // Since the DrawablePainter will be remembered and it implements RememberObserver, it
+    // will receive the necessary events
+    else -> DrawablePainter(drawable.mutate())
+  }
+}
+
+internal object EmptyPainter : Painter() {
+  override val intrinsicSize: Size get() = Size.Unspecified
+  override fun DrawScope.onDraw() {}
 }
