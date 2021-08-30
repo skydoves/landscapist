7b0f1b4c988a17227aa87833ad146da4cccca6f0
skydoves <skydoves@naver.com>
2021-08-30 19:02:18 +0900

    Refactor FlowCustomTarget to receive ProducerScope and try send result data

--- glide/src/main/java/com/skydoves/landscapist/glide/FlowCustomTarget.kt
+++ glide/src/main/java/com/skydoves/landscapist/glide/FlowCustomTarget.kt
@@ -16,46 +16,39 @@
 
 package com.skydoves.landscapist.glide
 
-import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
-import androidx.compose.ui.graphics.asImageBitmap
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.target.Target
 import com.bumptech.glide.request.transition.Transition
 import com.skydoves.landscapist.ImageLoadState
-import com.skydoves.landscapist.palette.BitmapPalette
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.channels.ProducerScope
+import kotlinx.coroutines.channels.trySendBlocking
 
 /**
- * FlowCustomTarget is a glide bitmap custom target which collects
- * [ImageLoadState] as a stateFlow.
+ * FlowCustomTarget is a [CustomTarget] for receiving Glide image results from network and handle states.
  */
 internal class FlowCustomTarget constructor(
-  private val bitmapPalette: BitmapPalette?
-) : CustomTarget<Bitmap>(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL) {
+  private val producerScope: ProducerScope<ImageLoadState>
+) : CustomTarget<Drawable>(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL) {
 
-  private val internalStateFlow = MutableStateFlow<ImageLoadState>(ImageLoadState.None)
-  val imageLoadStateFlow: StateFlow<ImageLoadState> get() = internalStateFlow
-
-  override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
-    this.internalStateFlow.value = ImageLoadState.Success(resource.asImageBitmap())
-    this.bitmapPalette?.generate(resource)
-  }
+  /** onResourceReady will be handled by [FlowRequestListener]. */
+  override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) = Unit
 
   override fun onLoadStarted(placeholder: Drawable?) {
     super.onLoadStarted(placeholder)
-    this.internalStateFlow.value = ImageLoadState.Loading(0f)
+    producerScope.trySendBlocking(ImageLoadState.Loading(0f))
   }
 
   override fun onLoadFailed(errorDrawable: Drawable?) {
     super.onLoadFailed(errorDrawable)
-    this.internalStateFlow.value = ImageLoadState.Failure(errorDrawable)
+    producerScope.trySendBlocking(ImageLoadState.Failure(errorDrawable))
+    producerScope.channel.close()
   }
 
   override fun onLoadCleared(placeholder: Drawable?) {
-    val bitmap = (placeholder as? BitmapDrawable)?.bitmap
-    this.internalStateFlow.value = ImageLoadState.Success(bitmap?.asImageBitmap())
+    // Glide wants to free up the resource, so we need to clear
+    // the result, otherwise we might draw a recycled bitmap later.
+    producerScope.trySendBlocking(ImageLoadState.None)
+    producerScope.channel.close()
   }
 }
