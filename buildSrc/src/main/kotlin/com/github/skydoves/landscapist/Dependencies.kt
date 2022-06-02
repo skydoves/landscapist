2fa1acc2f9cdded2084d0abfe56024b25abc4235
skydoves <skydoves@naver.com>
2022-06-02 21:03:25 +0900

    Bump AGP to 7.2.0

--- buildSrc/src/main/kotlin/com/github/skydoves/landscapist/Dependencies.kt
+++ buildSrc/src/main/kotlin/com/github/skydoves/landscapist/Dependencies.kt
@@ -1,7 +1,7 @@
 package com.github.skydoves.landscapist
 
 object Versions {
-    internal const val ANDROID_GRADLE_PLUGIN = "7.1.2"
+    internal const val ANDROID_GRADLE_PLUGIN = "7.2.0"
     internal const val ANDROID_GRADLE_SPOTLESS = "6.3.0"
     internal const val GRADLE_NEXUS_PUBLISH_PLUGIN = "1.1.0"
     internal const val KOTLIN = "1.6.10"
