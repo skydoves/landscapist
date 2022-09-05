447915f069781a3ccab27e87e3cf63c6d5daaf4f
skydoves <skydoves@naver.com>
2022-09-06 01:22:42 +0900

    Implement ShimmerPlugin and LocalShimmerPlugin

---
+++ landscapist-placeholder/src/main/kotlin/com/skydoves/landscapist/placeholder/shimmer/LocalShimmer.kt
@@ -0,0 +1,41 @@
+/*
+ * Designed and developed by 2020-2022 skydoves (Jaewoong Eum)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+@file:JvmName("LocalShimmerParams")
+@file:JvmMultifileClass
+@file:Suppress("unused")
+
+package com.skydoves.landscapist.placeholder.shimmer
+
+import androidx.compose.runtime.ProvidableCompositionLocal
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.graphics.Color
+import com.skydoves.landscapist.placeholder.shimmer.LocalShimmerProvider.defaultShimmerPlugin
+
+/**
+ * Local containing the preferred [ShimmerPlugin] for providing the same instance
+ * in the composable hierarchy.
+ */
+public val LocalShimmerPlugin: ProvidableCompositionLocal<ShimmerPlugin> =
+  compositionLocalOf { defaultShimmerPlugin() }
+
+/** A provider for taking the local instances related to the [Shimmer]. */
+internal object LocalShimmerProvider {
+
+  /** Returns the current or default [ShimmerPlugin] for the `shimmerParams` parameter. */
+  fun defaultShimmerPlugin(): ShimmerPlugin {
+    return ShimmerPlugin(baseColor = Color.DarkGray, highlightColor = Color.LightGray)
+  }
+}
---
+++ landscapist-placeholder/src/main/kotlin/com/skydoves/landscapist/placeholder/shimmer/ShimmerPlugin.kt
@@ -0,0 +1,57 @@
+/*
+ * Designed and developed by 2020-2022 skydoves (Jaewoong Eum)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.skydoves.landscapist.placeholder.shimmer
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import com.skydoves.landscapist.plugins.ImagePlugin
+
+/**
+ * Shimmer params holds attributes of the [Shimmer] composable.
+ *
+ * @param baseColor the base color of the content.
+ * @param highlightColor the shimmer's highlight color.
+ * @param intensity adjust the density of the highlight at the center.
+ * @param dropOff adjust the size of the fading edge of the highlight.
+ * @param tilt adjust an angle at which the highlight is tilted and measured in degrees.
+ * @param durationMillis a milli-second time to move the simmering effect from start to finish animation.
+ */
+@Immutable
+public data class ShimmerPlugin(
+  val baseColor: Color,
+  val highlightColor: Color,
+  val width: Dp? = null,
+  val intensity: Float = DefaultShimmerIntensity,
+  val dropOff: Float = DefaultShimmerDropOff,
+  val tilt: Float = DefaultShimmerTilt,
+  val durationMillis: Int = DefaultDurationMillis
+) : ImagePlugin.LoadingStatePlugin {
+
+  @Composable
+  override fun compose(): ImagePlugin = apply {
+    Shimmer(
+      baseColor = baseColor,
+      highlightColor = highlightColor,
+      shimmerWidth = width,
+      intensity = intensity,
+      dropOff = dropOff,
+      tilt = tilt,
+      durationMillis = durationMillis
+    )
+  }
+}
