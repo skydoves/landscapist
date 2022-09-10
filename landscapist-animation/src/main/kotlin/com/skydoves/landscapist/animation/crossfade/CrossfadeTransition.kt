3c1464f27846ab30956a732769e28e90f6c0e301
skydoves <skydoves@naver.com>
2022-09-10 14:12:56 +0900

    Apply spotless

--- landscapist-animation/src/main/kotlin/com/skydoves/landscapist/animation/crossfade/CrossfadeTransition.kt
+++ landscapist-animation/src/main/kotlin/com/skydoves/landscapist/animation/crossfade/CrossfadeTransition.kt
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.skydoves.landscapist.animation.crossfade
 
 import androidx.compose.animation.core.MutableTransitionState
