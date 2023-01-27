92277d40d88226b43ab1f4cec804fb7113ea4012
skydoves <skydoves@naver.com>
2023-01-27 23:21:38 +0900

    Add bash file about Baseline Profile Amender and run the bash on workflow

---
+++ bash/baseline-profile-amender.sh
@@ -0,0 +1,4 @@
+awk '/glide/' app/src/main/baseline-prof.txt > glide/src/main/baseline-prof.txt
+awk '/fresco|Fresco/' app/src/main/baseline-prof.txt > fresco/src/main/baseline-prof.txt
+awk '/coil/' app/src/main/baseline-prof.txt > coil/src/main/baseline-prof.txt
+awk '/landscapist/' app/src/main/baseline-prof.txt > landscapist/src/main/baseline-prof.txt
