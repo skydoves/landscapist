d728e617266c6ca959ee9512db88c10a6b425d04
skydoves <skydoves@naver.com>
2023-01-28 08:09:33 +0900

    Fix syntax for the baseline profile amender

--- bash/baseline-profile-amender.sh
+++ bash/baseline-profile-amender.sh
@@ -15,7 +15,7 @@
 awk '/glide/' app/src/main/baseline-prof.txt > glide/src/main/baseline-prof.txt
 awk '/fresco|Fresco/' app/src/main/baseline-prof.txt > fresco/src/main/baseline-prof.txt
 awk '/coil/' app/src/main/baseline-prof.txt > coil/src/main/baseline-prof.txt
-awk '/landscapist/ && !/glide/ && !/fresco/ && !/Fresco/ && !/coil/ && !/animation/ && !/palette/ && !/placeholder/ && !/transformation/' && !/benchmark/' app/src/main/baseline-prof.txt > landscapist/src/main/baseline-prof.txt
+awk '/landscapist/ && !/glide/ && !/fresco/ && !/Fresco/ && !/coil/ && !/animation/ && !/palette/ && !/placeholder/ && !/transformation/ && !/benchmark/' app/src/main/baseline-prof.txt > landscapist/src/main/baseline-prof.txt
 awk '/landscapist/ && /animation/' app/src/main/baseline-prof.txt > landscapist-animation/src/main/baseline-prof.txt
 awk '/landscapist/ && /palette/' app/src/main/baseline-prof.txt > landscapist-palette/src/main/baseline-prof.txt
 awk '/landscapist/ && /placeholder/' app/src/main/baseline-prof.txt > landscapist-placeholder/src/main/baseline-prof.txt
