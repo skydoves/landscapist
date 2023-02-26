4fb1a23d9e65137f5084962e3047ba436df92df4
skydoves <skydoves@naver.com>
2023-02-26 15:55:53 +0900

    Increment patchVersion

--- buildSrc/src/main/kotlin/com/github/skydoves/landscapist/Configuration.kt
+++ buildSrc/src/main/kotlin/com/github/skydoves/landscapist/Configuration.kt
@@ -22,9 +22,9 @@ object Configuration {
   const val minSdk = 21
   const val majorVersion = 2
   const val minorVersion = 1
-  const val patchVersion = 3
+  const val patchVersion = 4
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-  const val versionCode = 63
+  const val versionCode = 64
   const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
   const val artifactGroup = "com.github.skydoves"
 }
