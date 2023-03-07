f9ed00219a9f165923f04e933972b346e11634f9
skydoves <skydoves@naver.com>
2023-03-07 14:01:05 +0900

    Implement imageSemantics extension

---
+++ landscapist/src/main/kotlin/com/skydoves/landscapist/ImageSemantics.kt
@@ -0,0 +1,35 @@
+/*
+ * Designed and developed by 2020-2023 skydoves (Jaewoong Eum)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.skydoves.landscapist
+
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.role
+import androidx.compose.ui.semantics.semantics
+
+@Stable
+internal fun Modifier.imageSemantics(imageOptions: ImageOptions): Modifier {
+  return if (imageOptions.contentDescription != null) {
+    semantics {
+      this.contentDescription = imageOptions.contentDescription
+      this.role = Role.Image
+    }
+  } else {
+    this
+  }
+}
