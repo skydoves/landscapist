5fe75d470f18593b619645110965db409d2edab6
skydoves <skydoves@naver.com>
2024-01-11 00:36:58 +0900

    Fix benchmark demo

--- benchmark-landscapist-app/src/main/kotlin/com/skydoves/benchmark/landscapist/app/App.kt
+++ benchmark-landscapist-app/src/main/kotlin/com/skydoves/benchmark/landscapist/app/App.kt
@@ -21,7 +21,7 @@ import android.app.Application
 import android.content.Context
 import coil3.ImageLoader
 import coil3.SingletonImageLoader
-import coil3.fetch.NetworkFetcher
+import coil3.network.NetworkFetcher
 import com.facebook.drawee.backends.pipeline.Fresco
 import com.facebook.imagepipeline.backends.okhttp3.OkHttpImagePipelineConfigFactory
 import okhttp3.OkHttpClient
