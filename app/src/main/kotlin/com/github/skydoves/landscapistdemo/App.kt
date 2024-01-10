3c58d097cf99380f481c39c3affe98ca3559e082
skydoves <skydoves@naver.com>
2024-01-11 00:18:03 +0900

    Bump coil to 3.0.0-alpha02

--- app/src/main/kotlin/com/github/skydoves/landscapistdemo/App.kt
+++ app/src/main/kotlin/com/github/skydoves/landscapistdemo/App.kt
@@ -21,7 +21,7 @@ import android.content.Context
 import androidx.multidex.MultiDexApplication
 import coil3.ImageLoader
 import coil3.SingletonImageLoader
-import coil3.fetch.NetworkFetcher
+import coil3.network.NetworkFetcher
 import com.facebook.drawee.backends.pipeline.Fresco
 import com.facebook.imagepipeline.backends.okhttp3.OkHttpImagePipelineConfigFactory
 import dagger.hilt.android.HiltAndroidApp
