19d03b77225335d95ce7c2dddd0f2a5c62b60417
skydoves <skydoves@naver.com>
2024-10-29 21:47:16 +0900

    Back up to use jetbrains-compose 1.7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
-@Suppress("DSL_SCOPE_VIOLATION")
-plugins {
+@Suppress("DSL_SCOPE_VIOLATION") plugins {
   alias(libs.plugins.android.application) apply false
   alias(libs.plugins.android.library) apply false
   alias(libs.plugins.kotlin.android) apply false
