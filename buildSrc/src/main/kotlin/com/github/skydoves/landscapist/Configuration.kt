c0f91afc10c3124dae48fe306a0bda402c39da49
skydoves <skydoves@naver.com>
2024-11-05 18:07:09 +0900

    Compile & target SDk to 35

--- buildSrc/src/main/kotlin/com/github/skydoves/landscapist/Configuration.kt
+++ buildSrc/src/main/kotlin/com/github/skydoves/landscapist/Configuration.kt
@@ -17,8 +17,8 @@
 package com.github.skydoves.landscapist
 
 object Configuration {
-  const val compileSdk = 34
-  const val targetSdk = 34
+  const val compileSdk = 35
+  const val targetSdk = 35
   const val minSdk = 21
   const val minSdk24 = 24
   const val majorVersion = 2
