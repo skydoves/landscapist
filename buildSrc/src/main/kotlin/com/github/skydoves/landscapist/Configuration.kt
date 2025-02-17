36c24144f6fdd21e784467136e01834eb93ec3ae
skydoves <skydoves@naver.com>
2025-02-17 15:48:28 +0900

    Prepare for the next release

--- buildSrc/src/main/kotlin/com/github/skydoves/landscapist/Configuration.kt
+++ buildSrc/src/main/kotlin/com/github/skydoves/landscapist/Configuration.kt
@@ -23,9 +23,9 @@ object Configuration {
   const val minSdk24 = 24
   const val majorVersion = 2
   const val minorVersion = 4
-  const val patchVersion = 6
+  const val patchVersion = 7
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-  const val versionCode = 105
+  const val versionCode = 106
   const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
   const val artifactGroup = "com.github.skydoves"
 }
