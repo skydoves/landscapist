32fc83182547cf5f2b0cb4bad39ddab6b003b790
skydoves <skydoves@naver.com>
2025-06-03 14:08:35 +0900

    Update baseline profiles

--- benchmark-landscapist/build.gradle.kts
+++ benchmark-landscapist/build.gradle.kts
@@ -39,6 +39,10 @@ android {
     targetCompatibility = JavaVersion.VERSION_17
   }
 
+  kotlinOptions {
+    jvmTarget = "17"
+  }
+
   targetProjectPath = ":benchmark-landscapist-app"
 
   testOptions.managedDevices.devices {
